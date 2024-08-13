e64e247161f73289f9b5ca76d22e552e2d6b6851
Carlos Roberto <crrsj1@gmail.com>
2024-08-13 19:00:21 -0300

    implementando o projeto em arquitetura hexagonal

---
+++ src/main/java/com/arquitetura/hexagonal/ArquiteturaExagonalApplication.java
@@ -0,0 +1,13 @@
+package com.arquitetura.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaExagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaExagonalApplication.class, args);
+	}
+
+}
